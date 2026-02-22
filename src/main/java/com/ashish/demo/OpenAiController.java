2851f3e1f6da2c17d02f3e2d650fae144cbebdab
Ashish <ashishraj0609@gmail.com>
2026-02-22 23:05:14 +0530

    Uses ChatClient

--- src/main/java/com/ashish/demo/OpenAiController.java
+++ src/main/java/com/ashish/demo/OpenAiController.java
@@ -1,21 +1,25 @@
 package com.ashish.demo;
 
 import org.springframework.ai.azure.openai.AzureOpenAiChatModel;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OpenAiController {
-		private final AzureOpenAiChatModel chatModel;
+		private final ChatClient chatClient;
 
 		public OpenAiController(AzureOpenAiChatModel chatModel) { //constructor injection
-				this.chatModel = chatModel;
+				this.chatClient = ChatClient.create(chatModel);
 		}
 
 		@GetMapping("/api/{message}")
 		public String getResponse(@PathVariable String message){
-				String response = chatModel.call(message);
-				return response;
+				String response = chatClient.prompt(message)
+								.call()
+								.content();
+
+				return "with chat client :" + response;
 		}
 }
