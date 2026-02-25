e7f5af7ddf9dc591468758139fbfebb693649a4f
Ashish <ashishraj0609@gmail.com>
2026-02-26 02:53:44 +0530

    understand embeddings and log cosine similarity

--- src/main/java/com/ashish/demo/OpenAiController.java
+++ src/main/java/com/ashish/demo/OpenAiController.java
@@ -5,6 +5,8 @@ import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
 import org.springframework.ai.chat.prompt.Prompt;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.embedding.EmbeddingModel;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ai.chat.memory.InMemoryChatMemory;
@@ -16,6 +18,8 @@ import java.util.Map;
 public class OpenAiController {
 		private final ChatClient chatClient;
 
+		@Autowired
+		private  EmbeddingModel embeddingModel; // we can inject the embedding model if we want to use it for any purpose
 //		public OpenAiController(AzureOpenAiChatModel chatModel) { //constructor injection
 //				this.chatClient = ChatClient.create(chatModel);            // We need to do this when we multiple models are there and we want to inject a specific model. But if we have only one model then we can directly inject the chat client as shown in the constructor below.
 //		}
@@ -46,5 +50,27 @@ public class OpenAiController {
 
 				String response = chatClient.prompt(prompt).call().content();
 				return  response ;
+		}
+
+		@PostMapping("/api/embedding")
+		public float[] embedding (@RequestParam String text){
+				return embeddingModel.embed(text);
+		}
+
+		@PostMapping("/api/similarity")
+		public double similarity(@RequestParam String text1 , @RequestParam String text2){
+				float[] embedding1 = embeddingModel.embed(text1);
+				float[] embedding2 = embeddingModel.embed(text2);
+				double dotProduct = 0.0;
+				double norm1 = 0.0;
+				double norm2 = 0.0;
+				for (int i = 0; i < embedding1.length; i++) {
+						dotProduct += embedding1[i] * embedding2[i];
+						norm1 += Math.pow(embedding1[i], 2);
+						norm2 += Math.pow(embedding2[i], 2);
+				}
+
+				return dotProduct / (Math.sqrt(norm1) * Math.sqrt(norm2));
+
 		}
 }
